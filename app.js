74ef0d3ff0eb8d7400024b3ef348a1809781e5ce
Calvin Buka <63565027+buka1calvin@users.noreply.github.com>
2023-01-12 17:16:16 +0200

    javascript included

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 var toggleButton=document.getElementById("toggle");
 var navList=document.getElementById("listing");
+var indexes=document.getElementsByClassName("indexes");
+
+navList.classList.add('active')
 toggleButton.addEventListener("click",function(){
 navList.classList.toggle('active');
 })
+
+for (let btn of indexes){
+    btn.addEventListener("click",function(){
+        navList.classList.toggle('active');
+        })
+}
